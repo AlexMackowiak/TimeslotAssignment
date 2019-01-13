04fa9ff582288d5a8dbbea8a370d5b1bec0ec7ba
AlexMackowiak <Alex@Mackowiaks.com>
2019-01-12 20:35:48 -0600

    Added sanity tests that impossible preferences can't be solved

---
+++ tests/test_impossible.py
@@ -0,0 +1,29 @@
+import unittest
+from experimental_assignments_2 import assignModeratorsAndStudents
+
+class TestImpossible(unittest.TestCase):
+    """ Tests that no assignments can be made for inputs with no solution """
+
+    def test_too_many_students_no_solution(self):
+        """ Tests that no assignment can be made when one section would have to have 8 students """
+        test_data_dir = 'test_data/too_many_students/'
+        self.verify_no_solution(test_data_dir)
+
+    def test_too_few_students_no_solution(self):
+        """ Tests that no assignment can be made when one section would have to have 2 students """
+        test_data_dir = 'test_data/too_few_students/'
+        self.verify_no_solution(test_data_dir)
+
+    def test_too_few_mods_no_solution(self):
+        """ Tests that no assignment can be made when there are not enough moderators """
+        test_data_dir = 'test_data/too_few_mods/'
+        self.verify_no_solution(test_data_dir)
+
+    def verify_no_solution(self, test_data_dir):
+        mod_doodle_poll_csv_path = (test_data_dir + 'mod_preferences.csv')
+        mod_max_sections_csv_path = (test_data_dir + 'mod_max_sections.csv')
+        student_doodle_poll_csv_path = (test_data_dir + 'student_preferences.csv')
+
+        with self.assertRaises(AssertionError):
+            assignModeratorsAndStudents(mod_doodle_poll_csv_path, mod_max_sections_csv_path,
+                                        student_doodle_poll_csv_path)
